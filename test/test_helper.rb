f1bea5a2310bb9968c1c36a82256d7316cbc541e
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-05 09:30:23 -0700

    deleted tests for hotel block/ room and added to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,17 @@
 # Add simplecov
+require 'simplecov'
+SimpleCov.start do
+    add_filter 'test/' 
+end
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
 
+
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+require_relative '../lib/calendar'
+require_relative '../lib/hotel_booker'
+require_relative '../lib/reservation'
