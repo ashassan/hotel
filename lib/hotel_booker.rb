84c975200c31a76dc5c52c27ec1c742df4b9de4e
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-07 16:38:15 -0700

    added code for find_available_room, added code for create_reservation, changed @reservations and @rooms

--- lib/hotel_booker.rb
+++ lib/hotel_booker.rb
@@ -1,47 +1,50 @@
 
-require_relative 'reservation'
+require_relative "reservation"
 
-class HotelBooker 
-    attr_reader :rooms, :reservations 
-    
-    
-    def initialize
-        @rooms = []
-        @reservations = []
-    end 
-    
-    def  find_available_room(start_date, end_date)
-        Calendar.new(start_date, end_date)
+class HotelBooker
+  attr_reader :rooms, :reservations
 
-        
-    end 
-    def create_reservation(start_date, end_date)
-        find_available_room(start_date, end_date)
-       
-        Reservation.new()
+  def initialize
+    @rooms = (1..20).map { |i| i }
 
+    @reservations = {}
 
+    @rooms.each do |room|
+      @reservations[room] = []
     end
+  end
+
+  def find_available_room(start_date, end_date)
+    available_rooms = []
+    @reservations.each do |room, reservations|
+      if reservations.empty?
+        available_rooms = @rooms
+      else
+        reservations.each do |reservation|
+          date_range = Calendar.new(start_date, end_date)
+
+          if !date_range.overlap(reservation)
+            available_rooms << room
+          end
+        end
+      end
+    end
+
+    return available_rooms
+  end
+
+  def create_reservation(id, start_date, end_date)
+    found_rooms = find_available_room(start_date, end_date)
+    if found_rooms.empty?
+      raise ArgumentError " no available rooms "
+    else
+      new_reservation = Reservation.new(id: id, start_date: start_date, end_date: end_date, room: found_rooms.first)
+      @reservations[found_rooms.first] = @reservations[found_rooms.first] << new_reservation
     
-    
-    def find_reservation_by_date (date)
+      return new_reservation
+    end
+  end
 
-        
-        
-    end 
-
-
-
-
-    
-    
-    
-end 
-
-
-
-
-
-
-
-
+  #   def find_reservation_by_date(date)
+  #   end
+end
