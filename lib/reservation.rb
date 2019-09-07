a252425dcfa7986605e15b7c33e04e1698fb1d0f
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-06 23:39:35 -0700

    added total cost method and removed date range validation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,24 +1,15 @@
 class Reservation 
     attr_reader :id, :start_date, :end_date,:room 
-
+    
     def initialize (id:, start_date:, end_date:, room:)
         @id = id 
         @start_date = start_date
         @end_date = end_date 
-        @room = room 
-
-        if end_date < start_date 
-            raise ArgumentError.new "Invalid Date Range (end date is before start date)"
-
-        end 
-
+        @room = room       
     end 
-
-    # guest does not have to pay for last day 
+    
     def total_cost 
-
+        200 * (end_date - start_date).to_i 
     end 
-
-
-
+       
 end 
