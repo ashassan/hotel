69a23e0aa8895f0cb0b6d4f92b5864b626b81d5b
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-06 23:35:46 -0700

    added tests for total_cost method

---
+++ test/reservation_test.rb
@@ -0,0 +1,32 @@
+require_relative 'test_helper'
+
+describe "Wave 1 Reservation Tests" do 
+    describe "Initialize" do 
+        
+        
+    end 
+    
+    describe "total_cost method" do 
+        before do 
+           
+            @start_time = Date.new(2019,9,6)
+            @end_time = Date.new(2019,9,9)
+            @reservation = Reservation.new(id: 1, start_date: @start_time, end_date: @end_time, room: 5  )
+        
+        end 
+        it "checks that it returns an instance of an integer" do 
+           expect(@reservation.total_cost).must_be_instance_of Integer 
+
+        end 
+        
+        it "checks that it returns accurate calculation" do 
+            expect(@reservation.total_cost).must_equal 600
+
+        end 
+        
+        
+    
+    end 
+    
+    
+end 
