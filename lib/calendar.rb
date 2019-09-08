b918e26820f8bd560ac2f3d3f774812e74a7e592
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-08 11:47:54 -0700

    made overlap boolean zen

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,9 +1,16 @@
-class Calendar 
-def initialize 
+class Calendar
+  attr_reader :start_date, :end_date
 
-end 
+  def initialize(start_date:, end_date:)
+    @start_date = start_date
+    @end_date = end_date
 
+    if end_date < start_date
+      raise ArgumentError.new "Invalid Date Range (end date is before start date)"
+    end
+  end
 
-
-
-end 
+  def overlap?(current_reservation)
+    return !(current_reservation.start_date >= @end_date || current_reservation.end_date < @start_date)
+  end
+end
