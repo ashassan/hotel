30893c2e76531d77c136269d0e0549c86477142b
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-08 15:26:50 -0700

    fixed available rooms method

--- lib/hotel_booker.rb
+++ lib/hotel_booker.rb
@@ -18,14 +18,11 @@ class HotelBooker
     available_rooms = []
     @reservations.each do |room, reservations|
       if reservations.empty?
-        available_rooms = @rooms
+        available_rooms << room
       else
-        reservations.each do |reservation|
-          date_range = Calendar.new(start_date, end_date)
-
-          if !date_range.overlap(reservation)
-            available_rooms << room
-          end
+        date_range = Calendar.new(start_date: start_date, end_date: end_date)
+        if reservations.all? { |reservation| !date_range.overlap?(reservation) }
+          available_rooms << room
         end
       end
     end
@@ -35,16 +32,17 @@ class HotelBooker
 
   def create_reservation(id, start_date, end_date)
     found_rooms = find_available_room(start_date, end_date)
+
     if found_rooms.empty?
       raise ArgumentError " no available rooms "
     else
       new_reservation = Reservation.new(id: id, start_date: start_date, end_date: end_date, room: found_rooms.first)
       @reservations[found_rooms.first] = @reservations[found_rooms.first] << new_reservation
-    
+
       return new_reservation
     end
   end
 
-  #   def find_reservation_by_date(date)
-  #   end
+  def find_reservation_by_date(date)
+  end
 end
