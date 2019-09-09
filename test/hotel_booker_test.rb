590e501f91b5ab98d7809472aa55579cdcf9ad42
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-08 18:08:28 -0700

    added tests for available rooms method and find reservation by date

--- test/hotel_booker_test.rb
+++ test/hotel_booker_test.rb
@@ -23,13 +23,50 @@ describe "Wave 1 " do
       expect(reservation).must_be_instance_of Reservation
     end
 
-    it "checks if it adds the reservation " do
+    it "checks if new reservation is added to reservations" do
       hotel = HotelBooker.new
-      
+      new_res = hotel.create_reservation(1, Date.new(2019, 2, 5), Date.new(2019, 2, 9))
+      expect(hotel.reservations[1].length).must_equal 1
+    end
+    it "doesnt make conflicting reservations for the same room" do
+      hotel = HotelBooker.new
+      hotel.create_reservation(1, Date.new(2019, 2, 6), Date.new(2019, 2, 11))
+      hotel.create_reservation(4, Date.new(2019, 2, 7), Date.new(2019, 2, 10))
+
+      puts hotel.reservations
+      expect(hotel.reservations[1].length).must_equal 1
+      expect(hotel.reservations[2].length).must_equal 1
+    end
+    it "checks that new reservations start on checkout days" do
+      hotel = HotelBooker.new
+      hotel.create_reservation(1, Date.new(2019, 2, 6), Date.new(2019, 2, 11))
+      hotel.create_reservation(2, Date.new(2019, 2, 1), Date.new(2019, 2, 6))
+      expect(hotel.reservations[1].length).must_equal 2
+    end
   end
 
   describe "find_reservation_by_date" do
-    it "" do
+    before do
+      @hotel = HotelBooker.new
+      @hotel.create_reservation(1, Date.new(2019, 2, 6), Date.new(2019, 2, 11))
+      @hotel.create_reservation(2, Date.new(2019, 2, 1), Date.new(2019, 2, 6))
+      @hotel.create_reservation(3, Date.new(2019, 2, 3), Date.new(2019, 2, 12))
+      @hotel.create_reservation(4, Date.new(2019, 2, 7), Date.new(2019, 2, 10))
+      @hotel.create_reservation(5, Date.new(2019, 2, 8), Date.new(2019, 2, 13))
+    end
+    it "returns an array of reservations" do
+      found_reservations = @hotel.find_reservation_by_date(Date.new(2019, 2, 10))
+      expect(found_reservations).must_be_instance_of Array
+      found_reservations.each do |reservation|
+        expect(reservation).must_be_instance_of Reservation
+      end
+    end
+    it "all returned reservations inlcude the date" do
+      found_reservations = @hotel.find_reservation_by_date(Date.new(2019, 2, 6))
+      expect(found_reservations[0].id).must_equal 1
+      expect(found_reservations[1].id).must_equal 2
+      expect(found_reservations[2].id).must_equal 3
+      expect(found_reservations[3]).must_be_nil
     end
   end
 end
@@ -39,6 +76,27 @@ describe "Wave 2" do
       hotel = HotelBooker.new
       available_rooms = hotel.find_available_room(Date.new(2019, 2, 5), Date.new(2019, 2, 9))
       expect(available_rooms).must_be_instance_of Array
+    end
+
+    it "returns all the rooms when the array is empty" do
+      hotel = HotelBooker.new
+      available_rooms = hotel.find_available_room(Date.new(2019, 2, 5), Date.new(2019, 2, 9))
+      expect(available_rooms.length).must_equal 20
+    end
+
+    it "returns only empty rooms " do
+      hotel = HotelBooker.new
+
+      hotel.create_reservation(1, Date.new(2019, 2, 6), Date.new(2019, 2, 11))
+
+      hotel.create_reservation(2, Date.new(2019, 2, 1), Date.new(2019, 2, 8))
+
+      hotel.create_reservation(3, Date.new(2019, 2, 8), Date.new(2019, 2, 12))
+      hotel.create_reservation(4, Date.new(2019, 2, 11), Date.new(2019, 2, 14))
+      hotel.create_reservation(5, Date.new(2019, 2, 11), Date.new(2019, 2, 14))
+      available_rooms = hotel.find_available_room(Date.new(2019, 2, 5), Date.new(2019, 2, 9))
+      expect(available_rooms[0]).must_equal 3
+      expect(available_rooms.length).must_equal 18
     end
   end
 end
