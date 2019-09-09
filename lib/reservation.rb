8f88541645463c787ca06c7208b1240413dc784e
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-08 18:40:43 -0700

    fixed ruby syntax

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,16 +1,14 @@
-class Reservation 
-    attr_reader :id, :start_date, :end_date,:room 
-    
-    def initialize (id:, start_date:, end_date:, room:)
-        @id = id 
-        @start_date = start_date
-        @end_date = end_date 
-        @room = room    
-         
-    end 
-    
-    def total_cost 
-        200 * (@end_date - @start_date).to_i 
-    end 
-       
-end 
+class Reservation
+  attr_reader :id, :start_date, :end_date, :room
+
+  def initialize(id:, start_date:, end_date:, room:)
+    @id = id
+    @start_date = start_date
+    @end_date = end_date
+    @room = room
+  end
+
+  def total_cost
+    200 * (@end_date - @start_date).to_i
+  end
+end
