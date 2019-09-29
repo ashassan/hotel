0cee5c494fff6c20fae188dc31b1861bc76e2c5d
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-09-29 10:25:32 -0700

    added calculate days method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation
-  attr_reader :id, :start_date, :end_date, :room
+  attr_reader :id, :start_date, :end_date, :room, :date_range,:reservation_days
 
   def initialize(id:, start_date:, end_date:, room:)
     @id = id
@@ -9,6 +9,19 @@ class Reservation
   end
 
   def total_cost
-    200 * (@end_date - @start_date).to_i
+    200 * (end_date - start_date).to_i
   end
+
+  def calculate_days
+    days = [] 
+    current_date = end_date
+    while current_date >= start_date 
+      days << current_date 
+      current_date -= 1
+    end 
+    return days 
+  end 
+  
+
+
 end
